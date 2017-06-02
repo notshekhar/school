8ebdd3bb223e54c156750d5204fa3da0a0d85664
Shekhar Tyagi <shekhar.rdj@yahoo.com>
2017-06-02 21:14:31 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,10 @@
+    var load = document.getElementById('load');
+    var cont = document.getElementById('cont');
+    var htm = document.getElementById('loader');
+    htm.addEventListener('click', load);
+    function load(){
+         cont.style.display="none";
+         load.style.display="block";
+         
+    }
+    
