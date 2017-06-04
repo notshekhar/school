8270c45c04bcdee3056c4cfe545ccef4d0962ed2
Shekhar Tyagi <shekhar.rdj@yahoo.com>
2017-06-04 21:14:53 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
  $(document).ready(function(){
         $('#new').click(function(){
-		     $('#new').hide();
+		     $('#new').hide(400);
 			$('#editnew').show(500);
 			
 		});
@@ -9,6 +9,33 @@ $('#back').click(function(){
 	$('#editnew').hide(500);
 	$('#new').show(510);
 });		
+
+$('#put').focus(function(){
+	$('#nam').css({
+		"top":"-20px",
+		"font-size":"10px",
+		"transition-duration":"0.4s"
+	});
+
+}).blur(function(){
+	
+if($('#put').val().length > 1){
+	$('#nam').css({
+		"top":"-20px",
+		"font-size":"10px",
+		"transition-duration":"0.4s"
+	});
+}else{
+	$('#nam').css({
+		"top":"-9px",
+		"font-size":"20px",
+		"transition-duration":"0.4s"
+	});
+}
+});
+
+
+
   });
   
  
