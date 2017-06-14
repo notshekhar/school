1047abd1ca9809da5752ccd3743cc33bf7199178
Shekhar Tyagi <shekhar.rdj@yahoo.com>
2017-06-14 21:43:33 +0530

    Create app.js

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ var file = document.getElementById('imag');
 var uploaded = document.getElementById('sum');
 file.addEventListener('change', upload);
 
-function upload(){
-var selectedFile = this.value;
+function upload(event){
+var selectedFile = event.target.files[0];
 }
 uploaded.addEventListener('click', uploadFile);
 
 function uploadFile(){
 var path = document.getElementById("imag").value;
 var fileName = path.match(/[^\/\\]+$/);
-console.log(fileName[0]);
-
+  
+var storageRef = firebase.storage().ref('/stantschool/' + fileName);
+  var uploadTask = storageRef.put(selectedFile);
+  
+  storageRef.getDownloadURL().then(function(url) {
+        alert(url);
 }
