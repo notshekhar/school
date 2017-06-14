e7ebe320a9debbfca327bc6267c514854742f1d5
Shekhar Tyagi <shekhar.rdj@yahoo.com>
2017-06-14 22:02:25 +0530

    Create app.js

--- app.js
+++ app.js
@@ -14,7 +14,22 @@ var fileName = path.match(/[^\/\\]+$/);
 var storageRef = firebase.storage().ref('/stantschool/' + fileName);
   var uploadTask = storageRef.put(selectedFile);
   
-  storageRef.getDownloadURL().then(function(url) {
-        console.log(url);
-  });
+storageRef.child('images/stars.jpg').getDownloadURL().then(function(url) {
+  console.log(url);
+
+  // This can be downloaded directly:
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = 'blob';
+  xhr.onload = function(event) {
+    var blob = xhr.response;
+  };
+  xhr.open('GET', url);
+  xhr.send();
+
+  // Or inserted into an <img> element:
+  var img = document.getElementById('myimg');
+  img.src = url;
+}).catch(function(error) {
+  // Handle any errors
+});
 }
