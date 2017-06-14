21c2425185513527ee726f8d70930c1c7768e076
Shekhar Tyagi <shekhar.rdj@yahoo.com>
2017-06-14 21:07:41 +0530

    Create app.js

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ var file = document.getElementById('imag');
 file.addEventListener('change', upload);
 function upload(){
 var selectedFile = this.value
-console.log(selectedFile);
+}
+function uploadFile(){
+alert(selectedFile);
 }
