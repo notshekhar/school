0093f341e3064cbccd5194d11dde94b25f6a37bc
Shekhar Tyagi <shekhar.rdj@yahoo.com>
2017-06-14 21:21:22 +0530

    Create app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var file = document.getElementById('imag');
+var uploaded = document.getElementById('sum');
 file.addEventListener('change', upload);
+
 function upload(){
-var selectedFile = this.value
+var selectedFile = this.value;
 }
+uploaded.addEventListener('click', uploadFile);
+
 function uploadFile(){
-  var selected = var file = document.getElementById('imag').value
-alert(selected);
+var path = document.getElementById("whatever").value;
+var fileName = path.match(/[^\/\\]+$/);
+console.log(fileName);
+
 }
