afa5233b647e8167e590014dce7cac2d2b346890
Shekhar Tyagi <shekhar.rdj@yahoo.com>
2017-07-05 16:10:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var storageRef = firebase.storage().ref('/stantschool/' + fileName);
 
   
 // Create a reference to the file we want to download
-var starsRef = storageRef.child('images/stars.jpg');
+var starsRef = storageRef.child('stantschool/');
 
 // Get the download URL
 starsRef.getDownloadURL().then(function(url) {
