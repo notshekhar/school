1de035f35b2a0a3dbafb630431f8d3fced102b88
Shekhar Tyagi <shekhar.rdj@yahoo.com>
2017-07-05 16:08:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,13 +16,29 @@ var storageRef = firebase.storage().ref('/stantschool/' + fileName);
 
   
 // Create a reference to the file we want to download
-var starsRef = storageRef.child('stantschool/');
+var starsRef = storageRef.child('images/stars.jpg');
 
 // Get the download URL
 starsRef.getDownloadURL().then(function(url) {
-  console.log(url);
-});
-                    
+  // Insert url into an <img> tag to "download"
+}).catch(function(error) {
+
+  // A full list of error codes is available at
+  // https://firebase.google.com/docs/storage/web/handle-errors
+  switch (error.code) {
+    case 'storage/object_not_found':
+      // File doesn't exist
+      break;
+
+    case 'storage/unauthorized':
+      // User doesn't have permission to access the object
+      break;
+
+    case 'storage/canceled':
+      // User canceled the upload
+      break;
+  }
+});            
   
   
 
