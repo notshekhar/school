384199ce2644932ed927dfea6eff86fa2d8ce82d
Shekhar Tyagi <shekhar.rdj@yahoo.com>
2017-07-05 15:48:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,30 +14,10 @@ var fileName = path.match(/[^\/\\]+$/);
 var storageRef = firebase.storage().ref('/stantschool/' + fileName);
   var uploadTask = storageRef.put(selectedFile);
 
- storageRef.getDownloadURL().then(function(url) {
-   
   
-
-
- });
-   function writeNewPost() { 
-   var usr = "1";
-   var caption = document.getElementById('cap').value;
-   var name = document.getElementById('put').value;
-   
-   var postData = {
-    usr: usr,
-    name: name,
-    cap: caption,
-    image: url
-  };
-
-  var newPostKey = firebase.database().ref().child('/posts/').push().key;
-  var updates = {};
-  updates['/posts/'+newPostKey] = postData;
- return firebase.database().ref().update(updates);
-
-}   
+  
+  
+  
  
 
 }
