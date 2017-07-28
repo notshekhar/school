5507edb64c1b9639d375a58ab451ac734464e828
Shekhar Tyagi <shekhar.rdj@yahoo.com>
2017-07-28 15:50:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
 var file = document.getElementById('imag');
 var uploaded = document.getElementById('sum');
+
 file.addEventListener('change', upload);
 
 function upload(event){
 selectedFile = event.target.files[0];
 }
 uploaded.addEventListener('click', uploadFile);
-
+//uploading image and data in database
 function uploadFile(){
+  //uploading image
 var path = document.getElementById("imag").value;
 var fileName = path.match(/[^\/\\]+$/);
   
 var storageRef = firebase.storage().ref('/stantschool/' + fileName);
   var uploadTask = storageRef.put(selectedFile);
 
- function writeNewPost(url) {
-  // A post entry.
-  var postData = {
-    cap: document.getElementById('cap').value,
-    image: 'stantschool.png',
-    name: document.getElementById('cap').value,
-    usr: 1
-  };
-
-  // Get a key for a new Post.
-  var newPostKey = firebase.database().ref('posts/');
-
+//sending data to database
+ var newPostKey = firebase.database().ref('posts/');
+var postData{
+  cap: document.getElementById('cap').value,
+  img: 'url'
+  usr: document.getElementById('put').value,
+}
  newPostKey.push(postData);
+
+
 }           
   
   
 
-}
